00cfcf3d9b076c4b2be8b74b16588c3baca15c94
nofalx <nofal.ahmad@outlook.com>
2021-04-13 15:48:13 +0400

    updated schema

--- main.tf
+++ main.tf
@@ -22,16 +22,30 @@ resource "google_compute_project_metadata" "default_project_metadata" {
   }
 }
 
-data "template_file" "add_ssh_keys" {
-  template = file("./scripts/add-ssh-web-app.yaml")
-  vars = {
-    // public_ssh_keys_list : jsonencode(var.public_ssh_keys_list)
-  }
-}
+# data "template_file" "add_ssh_keys" {
+#   template = file("./scripts/add-ssh-web-app.yaml")
+#   vars = {
+#     // public_ssh_keys_list : jsonencode(var.public_ssh_keys_list)
+#   }
+# }
 
 resource "google_compute_network" "vpc_network_one" {
   name = "terraform-network1"
 }
+
+# resource "google_compute_disk" "chain_disk_1" {
+# name = "chain-disk-1"
+# size = 80
+# type = "pd-ssd"
+# labels = {
+#   environment = "dev"
+# }
+# }
+
+# resource "google_compute_attached_disk" "default" {
+#   disk     = google_compute_disk.chain_disk_1.id
+#   instance = google_compute_instance.vm_instance_one.id
+# }
 
 resource "google_compute_instance" "vm_instance_one" {
   name         = "terraform-instance1"
@@ -46,6 +60,11 @@ resource "google_compute_instance" "vm_instance_one" {
   boot_disk {
     initialize_params {
       image = "ubuntu-2004-lts"
+      size  = 80
+      type  = "pd-ssd"
+      labels = {
+        environment = "dev"
+      }
     }
   }
 
@@ -77,6 +96,35 @@ resource "google_compute_firewall" "default" {
 
 }
 
-resource "google_storage_bucket" "chain_backup_bucket" {
-  name = "chain_backup_bucket"
+
+
+resource "google_compute_resource_policy" "chain_snapshot_schedule" {
+  name = "chain-snapshot-schedule"
+
+  snapshot_schedule_policy {
+    schedule {
+      daily_schedule {
+        days_in_cycle = 1
+        start_time    = "02:00"
+      }
+    }
+    retention_policy {
+      max_retention_days    = 7
+      on_source_disk_delete = "APPLY_RETENTION_POLICY"
+    }
+    snapshot_properties {
+      labels = {
+        app = "chain"
+      }
+    }
+  }
 }
+
+resource "google_compute_disk_resource_policy_attachment" "attachment_bootdisk" {
+  name = google_compute_resource_policy.chain_snapshot_schedule.name
+  disk = google_compute_instance.vm_instance_one.name
+}
+# resource "google_compute_disk_resource_policy_attachment" "attachment_new_disk" {
+#   name = google_compute_resource_policy.chain_snapshot_schedule.name
+#   disk = google_compute_disk.chain_disk_1.name
+# }
