e3e89652db267a3fcd7984fb59bae221f160c8d9
nofalx <nofal.ahmad@outlook.com>
2021-04-20 13:23:51 +0400

    fixed bucket

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,9 +1,11 @@
+resource "google_storage_bucket" "artifacts_bucket" {
+  name = "nofal-nodle-artifacts"
+}
+
 resource "google_storage_bucket_access_control" "artifacts_public_rule" {
-  bucket = google_storage_bucket.bucket.name
+  bucket = google_storage_bucket.artifacts_bucket.name
   role   = "READER"
   entity = "allUsers"
 }
 
-resource "google_storage_bucket" "artifacts_bucket" {
-  name = "nofal-nodle-artifacts"
-}
+
